47cdd41a813fc6f3165ffe5f6e6e674d41752d91
Jovisl <jovis.lelue@ynov.com>
2023-08-15 22:18:24 +0200

    finito

--- index/menuBurger.js
+++ index/menuBurger.js
@@ -1,8 +1,6 @@
+const mobileMenuButton = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
 
-    const mobileMenuButton = document.getElementById("mobile-menu");
-    const navList = document.querySelector(".nav-list");
-
-    mobileMenuButton.addEventListener("click", function() {
-        navList.classList.toggle("active");
-    });
-
+mobileMenuButton.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
